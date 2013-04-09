bafd41c7b0d0ac76ecdd6119d216ed780e3f5d2f
ianbattersby <ian.battersby@gmail.com>
2013-04-09 07:46:00 +0100

    Updated to rake build process.

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Runtime.CompilerServices;
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyCompany("Ian Battersby")]
+[assembly: AssemblyProduct("ScriptCs.SimpleWeb")]
+[assembly: AssemblyCopyright("Copyright (C) Ian Battersby 2013")]
+[assembly: AssemblyTrademark("7b352e095a8205669fb9eb78822f30ce924d43e1")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.0.2.50745")]
+[assembly: AssemblyFileVersion("0.0.2.50745")]
+
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyInformationalVersion("0.0.2.50745")]
